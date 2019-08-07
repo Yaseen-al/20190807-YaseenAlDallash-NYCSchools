0c165b7093be12271ce24b1a5e4bebbe9caa0394
Yaseen AlDallash <yaseenmeald@hotmail.com>
2019-08-07 15:53:25 -0400

    - Added Custom fonts. - Updated `info.plist` with custom fonts

---
+++ 20190807-YaseenAlDallash-NYCSchools/20190807-YaseenAlDallash-NYCSchools/Utilities/Extension/UIFont+Extension.swift
@@ -0,0 +1,33 @@
+//
+//  UIFont+Extension.swift
+//  YourBank
+//
+//  Created by Yaseen Al Dallash on 7/31/19.
+//  Copyright © 2019 Yaseen Al Dallash. All rights reserved.
+//
+
+import UIKit
+
+enum CustomFontName: String {
+    case bold           = "SFUIText-Bold"
+    case boldItalic     = "SFUIText-BoldItalic"
+    case heavy          = "SFUIText-Heavy"
+    case heavyItalic    = "SFUIText-HeavyItalic"
+    case light          = "SFUIText-Light"
+    case lightItalic    = "SFUIText-LightItalic"
+    case medium         = "SFUIText-Medium"
+    case mediumItalic   = "SFUIText-MediumItalic"
+    case regular        = "SFUIText-Regular"
+    case regularItalic  = "SFUIText-RegularItalic"
+    case semiBold       = "SFUIText-Semibold"
+    case semiBoldItalic = "SFUIText-SemiboldItalic"
+}
+
+
+extension UIFont {
+    
+    class func custom(name: CustomFontName, size: CGFloat) -> UIFont {
+        let fontDiscreption = UIFontDescriptor(name: name.rawValue, size: size)
+        return UIFont(descriptor: fontDiscreption, size: size)
+    }
+}
