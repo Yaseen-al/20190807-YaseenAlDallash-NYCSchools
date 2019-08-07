e02a1488ca92f9a20211015f4e334e3af56bef7f
Yaseen AlDallash <yaseenmeald@hotmail.com>
2019-08-07 13:41:07 -0400

    Initiated project files and structure

---
+++ 20190807-YaseenAlDallash-NYCSchools/20190807-YaseenAlDallash-NYCSchools/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  20190807-YaseenAlDallash-NYCSchools
+//
+//  Created by Yaseen Al Dallash on 8/7/19.
+//  Copyright © 2019 Yaseen AlDallash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
