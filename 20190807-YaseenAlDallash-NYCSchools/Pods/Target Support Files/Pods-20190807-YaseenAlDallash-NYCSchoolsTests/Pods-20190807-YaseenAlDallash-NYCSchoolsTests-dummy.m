3a8c32de3db8f86a4bfca6aeaa3902dca2f0c754
Yaseen AlDallash <yaseenmeald@hotmail.com>
2019-08-08 23:44:33 -0400

    - Added Pods for requirments.

---
+++ 20190807-YaseenAlDallash-NYCSchools/Pods/Target Support Files/Pods-20190807-YaseenAlDallash-NYCSchoolsTests/Pods-20190807-YaseenAlDallash-NYCSchoolsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_20190807_YaseenAlDallash_NYCSchoolsTests : NSObject
+@end
+@implementation PodsDummy_Pods_20190807_YaseenAlDallash_NYCSchoolsTests
+@end
---
+++ 20190807-YaseenAlDallash-NYCSchools/Pods/Target Support Files/Pods-20190807-YaseenAlDallash-NYCSchoolsUITests/Pods-20190807-YaseenAlDallash-NYCSchoolsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_20190807_YaseenAlDallash_NYCSchoolsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_20190807_YaseenAlDallash_NYCSchoolsUITests
+@end
